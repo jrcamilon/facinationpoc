f753f8fb6f35cd99d551ac4b0b142d9990d8469b
unknown <jishak@infosol.com>
2018-07-12 19:54:13 -0700

    Fixed routes to exclude a '/' from one variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   infosolApi: 'http://vm1.infosol.com:8551',
   nodeJs: {
-    local: 'http://localhost:8012/',
-    vm3: 'http://vm3.infosol.com:8012/'
+    local: 'http://localhost:8012',
+    vm3: 'http://vm3.infosol.com:8012'
   }
 
 };
