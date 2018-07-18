80fc8ff8e30a9577fd1b86e724d5d7e379edc224
jrcamilon <jrcamilon@gmail.com>
2018-07-18 01:07:14 -0700

    Added Colors to Bar Chart for Primary Advantages and Added Gender Bar Chart

--- src/app/modules/home/components/modal/modal.component.ts
+++ src/app/modules/home/components/modal/modal.component.ts
@@ -24,6 +24,19 @@ export class ModalComponent implements OnInit {
   primaryData: any[];
   primaryTitle = 'Primary Advantages Total Scores';
 
+  genderCateogires: string[];
+  genderData: any[];
+  genderTital = 'Gender Distribution';
+
+  advantageColors = [
+    { advantage: 'innovation', color: '#EDA716'},
+    { advantage: 'passion', color: '#B74A2A' },
+    { advantage: 'power', color: '#82181B' },
+    { advantage: 'prestige', color: '#472656'},
+    { advantage: 'trust', color: '#223D6E'},
+    { advantage: 'mystique', color: '#005B5D'},
+    { advantage: 'alert', color: '#285A17'}
+  ];
 
 
   languageOf: string;
@@ -53,23 +66,27 @@ export class ModalComponent implements OnInit {
         total[adv] = (total[adv] || 0) + item[adv];
       });
     });
+    const objArray = _.map(total, function(v, k, a) { return _.pick(a, k); });
+    const dataWithColors = objArray.map(ele => {
+      return new Object({value: Object.values(ele)[0], color: this.getColor(Object.keys(ele)[0])});
+    });
 
-    const data = [total['innovation'], total['passion'], total['power'],
-    total['prestige'], total['trust'], total['mystique'], total['alert'] ];
+    const categories = objArray.map(ele => {
+      return Object.keys(ele)[0];
+    });
 
-    console.log(_advantages);
-    console.log(data);
+    // assignt the primary categories and data with colors to pass to the bar graph
+    this.primaryCategories = categories;
+    this.primaryData = dataWithColors;
 
-    this.primaryCategories = _advantages;
-    this.primaryData = data;
+    this.organizeByGender();
 
-    console.log(this.data);
-    console.log(this.index[0]);
+    // set title and other data related to the archetype
     this.title = this.data[0]['archetype'] !== undefined ? this.data[0]['archetype'] + " - " : 'No Data';
-    const archIndex = _.findIndex(NodejsApiService.archetypeData, (a) => a.boxkey === this.index)
+    const archIndex = _.findIndex(NodejsApiService.archetypeData, (a) => a.boxkey == this.index)
     this.title += NodejsApiService.archetypeData[archIndex].adjectives;
 
-    const dataIndex = _.findIndex(NodejsApiService.boxPrimaryData, (a) => a.key === this.index[0])
+    const dataIndex = _.findIndex(NodejsApiService.boxPrimaryData, (a) => a.key == this.index[0])
     const primaryData =  NodejsApiService.boxPrimaryData[dataIndex];
 
     this.languageOf = primaryData.languageOf;
@@ -78,6 +95,30 @@ export class ModalComponent implements OnInit {
     this.otherLeaders = primaryData.otherLeaders;
     this.overview = primaryData.overview;
 
+  }
+
+  public organizeByGender() {
+    const genderGrouped = _.groupBy(this.data, function(item) { return  item.gender; });
+    const groupGender = Object.keys(genderGrouped).map(i => genderGrouped[i]);
+
+    const genderObjectsArray = [];
+    const group = Object.keys(genderGrouped);
+
+    for (let i = 0; i < groupGender.length; i++) {
+      genderObjectsArray.push(new Object({key: group[i], value: groupGender[i]}));
+    }
+
+    const organizedByGender = genderObjectsArray.map(ele => {
+      return new Object({value: ele.value.length, color: ele.key === 'male' ? '#003F7F' : '#FF017E'});
+    });
+
+    this.genderCateogires = group;
+    this.genderData = organizedByGender;
+  }
+
+  public getColor(_advantage: String) {
+    const index = this.advantageColors.findIndex(x => x.advantage === _advantage);
+    return this.advantageColors[index].color;
   }
 
 
