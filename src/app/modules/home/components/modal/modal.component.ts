89fed5117e52fa4d852c50eb5439220eb83d18a8
unknown <jishak@infosol.com>
2018-07-17 20:22:34 -0700

    Added the adjectives to the title bar in the modal window

--- src/app/modules/home/components/modal/modal.component.ts
+++ src/app/modules/home/components/modal/modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { NodejsApiService } from 'src/app/services/nodejs-api.service';
+import * as _ from 'lodash';
+import { arch } from 'os';
 
 
 @Component({
@@ -28,7 +30,10 @@ export class ModalComponent implements OnInit {
 
   ngOnInit() {
     console.log(this.data);
-    this.title = this.data[0]['archetype'] !== undefined ? this.data[0]['archetype'] : 'No Data';
+    this.title = this.data[0]['archetype'] !== undefined ? this.data[0]['archetype'] + " - " : 'No Data';
+    let archIndex = _.findIndex(NodejsApiService.archetypeData,(a) => { return a.boxkey == this.index})
+    this.title +=NodejsApiService.archetypeData[archIndex].adjectives;
+    console.log(this.title);
   }
 
 
