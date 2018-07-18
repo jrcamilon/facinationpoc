c00d6075c3d7bfcab16c9bd4d0b77617743f780b
jrcamilon <jrcamilon@gmail.com>
2018-07-18 15:56:49 -0700

    Changed colors on pie, ate the pie, pie was tasty

--- src/app/modules/home/components/charts/pie/pie.component.ts
+++ src/app/modules/home/components/charts/pie/pie.component.ts
@@ -12,10 +12,11 @@ export class PieComponent implements OnInit {
   public pieData: any = [
     { category: 'Male', value: 0.42 },
     { category: 'Female', value: 0.58 }
-  ]
+  ];
 
   @Input() males: any;
   @Input() females: any;
+  @Input() title: string;
 
 
   @Input() type: any;
@@ -26,13 +27,13 @@ export class PieComponent implements OnInit {
     console.log(this.males);
     console.log(this.females);
     console.log(this.type);
-    if(this.type == "archetype"){
-      this.pieData[0].value = this.males / (this.males+this.females);
-      this.pieData[1].value = this.females / (this.males+this.females);
-    } else{
+    if (this.type === 'archetype') {
+      this.pieData[0].value = this.males / (this.males + this.females);
+      this.pieData[1].value = this.females / (this.males + this.females);
+    } else {
       this.pieData[0].value = this.males / (NodejsApiService.totalMales);
       this.pieData[1].value = this.females / (NodejsApiService.totalFemales);
- 
+    }
   }
 
 }
